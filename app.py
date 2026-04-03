c28f49a57e57f3d88d920ae70c58fe5fb550187b
ik4881158-art <ik4881158@gmail.com>
2026-04-03 21:41:12 +0400

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def index():
     return render_template_string(HTML_TEMPLATE, results=results, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render передает порт в переменной окружения PORT
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
